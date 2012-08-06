647542753c2a516aed52e0f8f645b9a41b3aebb3
Bertrand Bordage <bordage.bertrand@gmail.com>
2012-08-06 16:25:41 +0200

    Version 0.2.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-terms',
-    version='0.1',
+    version='0.2',
     author='Bertrand Bordage',
     author_email='bordage.bertrand@gmail.com',
     url='https://github.com/BertrandBordage/django-terms',
