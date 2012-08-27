5237cb7f1339eb13b4c01f1c3611448a8f865726
Bertrand Bordage <bordage.bertrand@gmail.com>
2012-08-27 23:03:28 +0200

    Make sure the filter arg is a string.

--- terms/templatetags/terms.py
+++ terms/templatetags/terms.py
@@ -1,12 +1,14 @@
 # coding: utf-8
 
 from django.template import Library
+from django.template.defaultfilters import stringfilter
 from ..html import TermsHTMLReconstructor
 
 register = Library()
 
 
 @register.filter
+@stringfilter
 def replace_terms(html):
     parser = TermsHTMLReconstructor()
     parser.feed(html)
