a2da9bb97c0d007f70295e3898f1b517e225f485
jssevestre <contact@jssevestre.net>
2012-12-13 15:29:14 +0100

    [ADD] a simple cms_plugin_processor for django-cms

---
+++ terms/cms_plugin_processors.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+from terms.html import TermsHTMLReconstructor
+import re
+
+
+def TermsProcessor(instance, placeholder, rendered_content, original_context):
+    """
+    This processor mark all terms in all placeholders plugins
+    """
+
+    # fix <br> "can't find end tag" error
+    rendered_content = re.sub('<br>','<br/>',rendered_content)
+
+    parser = TermsHTMLReconstructor()
+    parser.feed(rendered_content)
+
+    from django.template import Context, Template
+    t = Template('{{ content|safe }}')
+    c = Context({
+        'content': parser.out,
+    })
+    return t.render(c)
