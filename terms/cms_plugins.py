1622ffd07de2cd4d0d4d712733aa25830c12535c
js.sevestre <sjs@jssevestre.net>
2012-12-13 17:25:47 +0100

    Update terms/cms_plugins.py

--- terms/cms_plugins.py
+++ terms/cms_plugins.py
@@ -2,7 +2,8 @@ from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
 from cms.models.pluginmodel import CMSPlugin
 from django.utils.translation import ugettext_lazy as _
-from terms.models import Term
+from .models import Term
+
 
 class TermsIndexPlugin(CMSPluginBase):
     model = CMSPlugin
