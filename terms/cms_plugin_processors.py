3c6c2148e2dc3721529f665d7a0b19fd3efc674d
jssevestre <contact@jssevestre.net>
2012-12-13 16:55:25 +0100

    [FIX] don't process term index plugin output

--- terms/cms_plugin_processors.py
+++ terms/cms_plugin_processors.py
@@ -7,18 +7,21 @@ import re
 
 def TermsProcessor(instance, placeholder, rendered_content, original_context):
     """
-    This processor mark all terms in all placeholders plugins
+    This processor mark all terms in all placeholders plugins except termsplugins
     """
+    if 'terms' in original_context:
+        return rendered_content
+    else:
+        # fix <br> "can't find end tag" error
+        rendered_content = re.sub('<br>','<br/>',rendered_content)
 
-    # fix <br> "can't find end tag" error
-    rendered_content = re.sub('<br>','<br/>',rendered_content)
 
-    parser = TermsHTMLReconstructor()
-    parser.feed(rendered_content)
+        parser = TermsHTMLReconstructor()
+        parser.feed(rendered_content)
 
-    from django.template import Context, Template
-    t = Template('{{ content|safe }}')
-    c = Context({
-        'content': parser.out,
-    })
-    return t.render(c)
+        from django.template import Context, Template
+        t = Template('{{ content|safe }}')
+        c = Context({
+            'content': parser.out,
+        })
+        return t.render(c)
