454251ed47eba9b6228d2935a0f4b150fcb12e89
Bertrand Bordage <bordage.bertrand@gmail.com>
2013-06-30 16:08:49 +0200

    Cleanup.

--- terms/html.py
+++ terms/html.py
@@ -2,7 +2,6 @@
 
 import re
 from bs4 import BeautifulSoup
-from bs4.element import NavigableString
 from .models import Term
 from .settings import TERMS_IGNORED_TAGS, TERMS_IGNORED_CLASSES, \
                       TERMS_IGNORED_IDS, TERMS_REPLACE_FIRST_ONLY
@@ -40,19 +39,11 @@ def replace_terms(replace_dict, variants_dict, replace_regexp__sub, html):
     return replace_regexp__sub(translate, html)
 
 
-def is_navigable_string(navigable_string):
-    return isinstance(navigable_string, NavigableString)
-
-
-def html_content_iterator(parent_tag, replace_regexp):
-    out = []
-    for tag in parent_tag.find_all(text=replace_regexp):
-        if tag.find_parent(name=TAGS_REGEXP) \
-            or tag.find_parent(class_=CLASSES_REGEXP) \
-                or tag.find_parent(id=IDS_REGEXP):
-            continue
-        out.append(tag)
-    return out
+def get_interesting_contents(parent_tag, replace_regexp):
+    return [tag for tag in parent_tag.find_all(text=replace_regexp)
+            if not (tag.find_parent(name=TAGS_REGEXP)
+                    or tag.find_parent(class_=CLASSES_REGEXP)
+                    or tag.find_parent(id=IDS_REGEXP))]
 
 
 def str_to_soup(html):
@@ -66,7 +57,7 @@ def replace_in_html(html):
     replace_regexp = Term.objects.replace_regexp()
     replace_regexp__sub = replace_regexp.sub
     soup = str_to_soup(html)
-    for content in html_content_iterator(soup, replace_regexp):
+    for content in get_interesting_contents(soup, replace_regexp):
         new_content = str_to_soup(replace_terms(
             replace_dict, variants_dict, replace_regexp__sub, content))
         content.replace_with(new_content)
