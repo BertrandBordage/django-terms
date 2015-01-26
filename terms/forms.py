e55cfda7fdb0944244c7883f88cdb361cf38888e
Bertrand Bordage <bordage.bertrand@gmail.com>
2015-01-26 21:26:02 +0100

    Fixes a deprecation.

--- terms/forms.py
+++ terms/forms.py
@@ -47,3 +47,4 @@ class TermForm(ModelForm):
             'name': TextInput(attrs={'size': 120}),
             'definition': Textarea(),
         }
+        fields = ('name', 'case_sensitive', 'definition', 'url')
