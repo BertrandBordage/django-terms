e3558cfbb50c7b50c9cd5ea5a564bd3160f8b5a1
Bertrand Bordage <bordage.bertrand@gmail.com>
2015-01-27 18:14:15 +0100

    Adds some tests for a new edge case encountered.

--- terms/tests/html/valid_html.py
+++ terms/tests/html/valid_html.py
@@ -15,6 +15,9 @@ def read_file(filename):
 
 
 class ValidHTMLTestCase(TestCase):
+    def setUp(self):
+        self.term = Term.objects.create(name='était', url='github.com')
+
     def testEmptyString(self):
         self.assertHTMLEqual(replace_terms(''), '')
 
@@ -35,8 +38,6 @@ class ValidHTMLTestCase(TestCase):
         self.assertHTMLEqual(html, new_html_w_extra_spaces)
 
     def testUnicode(self):
-        Term.objects.create(name='était', url='github.com')
-
         control = 'Il <a href="github.com">était</a> une fois…'
 
         text = 'Il était une fois…'
@@ -44,3 +45,35 @@ class ValidHTMLTestCase(TestCase):
 
         html = 'Il &eacute;tait une fois&hellip;'
         self.assertEqual(replace_terms(html), control)
+
+    def testText(self):
+        self.assertHTMLEqual(replace_terms('était'),
+                             '<a href="github.com">était</a>')
+
+    def testTextAndNode(self):
+        self.assertHTMLEqual(replace_terms('était <em>une fois</em>'),
+                             '<a href="github.com">était</a> <em>une fois</em>')
+
+    def testSingleParagraph(self):
+        self.assertHTMLEqual(replace_terms('<p>était</p>'),
+                             '<p><a href="github.com">était</a></p>')
+
+    def testSingleAttributedParagraph(self):
+        self.assertHTMLEqual(replace_terms('<p id="test">était</p>'),
+                             '<p id="test"><a href="github.com">était</a></p>')
+
+    def testMultipleParagraphs(self):
+        html = ('<p>First paragraph</p>'
+                '<p>Il était une fois</p>')
+        control = ('<p>First paragraph</p>'
+                   '<p>Il <a href="github.com">était</a> une fois</p>')
+        self.assertHTMLEqual(replace_terms(html), control)
+
+    def testMultipleAttributedParagraphs(self):
+        html = (
+            '<p id="test1">First paragraph</p>'
+            '<p id="test2">Il était une fois</p>')
+        control = (
+            '<p id="test1">First paragraph</p>'
+            '<p id="test2">Il <a href="github.com">était</a> une fois</p>')
+        self.assertHTMLEqual(replace_terms(html), control)
